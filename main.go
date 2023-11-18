06bbeed4ff53c288329a70d4215cb809541f7851
ShafqatShahriar <shafqatarefin@gmail.com>
2023-11-18 19:20:36 +0600

    primitive data types

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main(){
+
+var name string
+var age int 
+var result float32
+var chr rune
+var isFound bool
+    fmt.Println(name)
+    fmt.Println(age)
+    fmt.Println(result)
+    fmt.Println(chr)
+    fmt.Println(isFound)
+    fmt.Println(true && true)
+    fmt.Println(true && false)
+    fmt.Println(true || true)
+    fmt.Println(true || false)
+    fmt.Println(!true)
+
+var char rune
+chr = 'A'
+fmt.Println(char)
+fmt.Printf("%c", char)
+
+}
