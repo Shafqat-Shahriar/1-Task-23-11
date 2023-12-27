8abffcffd651cecec179af2adeb0b89d453abcc4
Shafqat-Shahriar <60550696+Shafqat-Shahriar@users.noreply.github.com>
2023-12-27 23:30:22 +0600

    practice

---
+++ GO_PRactice_basics/Basic_Coding_Practice/variables.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    var a = "initial"
+    fmt.Println(a)
+
+    var b, c int = 1, 2
+    fmt.Println(b, c)
+
+    var d = true
+    fmt.Println(d)
+
+    var e int
+    fmt.Println(e)
+
+    f := "apple"
+    fmt.Println("Initial value of f:", f)
+
+    // Change the value of f
+    f = "banana"
+    fmt.Println("Updated value of f:", f)
+}
