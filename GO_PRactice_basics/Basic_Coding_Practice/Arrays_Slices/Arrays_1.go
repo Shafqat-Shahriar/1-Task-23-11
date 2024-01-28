df44f309e6971420a9f32b132c8d2e07a1c44e62
Shafqat-Shahriar <60550696+Shafqat-Shahriar@users.noreply.github.com>
2024-01-28 20:10:29 +0600

    Update Arrays_1.go

--- GO_PRactice_basics/Basic_Coding_Practice/Arrays_Slices/Arrays_1.go
+++ GO_PRactice_basics/Basic_Coding_Practice/Arrays_Slices/Arrays_1.go
@@ -8,14 +8,7 @@ primeNumbers = append(primeNumbers,11)
 
 fmt.Println(primeNumbers)
 
-var Doxc [2][3]int
-    for i := 0; i < 2; i++ {
-        for j := 0; j < 3; j++ {
-            Doxc[i][j] = i + j
-        }
-    }
-    fmt.Println("2d: ", Doxc)
-
+primeNumbers = append(primeNumbers[0:1], append([]int{3}, primeNumber[1:]
   
 }
 
